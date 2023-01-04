52b92eb70e22d2b1553f9f8db31167b703e97076
Kanyana <ktoadithep@gmail.com>
2023-01-04 09:54:09 +0700

    0

--- routes/customer.route.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-    const customer = require("../controllers/customer.controller.js")
-
-    app.get('/',customer.index);
-    app.get('/api/customer',customer.findAll);
-}
